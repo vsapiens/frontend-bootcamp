efe261a240637c101d4c1f304aef60f57645f421
vsapiens <erick.frank@icloud.com>
2019-05-25 18:51:26 -0500

    Component fractioning

--- market-app/src/components/counter.jsx
+++ market-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tags", "tag2", "tag3"]
   };
   /*
@@ -17,24 +17,21 @@ class Counter extends Component {
   }
   */
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
         <button
-          onClick={() => {
-            this.handleIncrement(product);
-          }}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         {/*this.state.tags.length === 0 && "Please add one tag!"*/}
-        {this.renderTags()}
       </div>
     );
   }
@@ -52,12 +49,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
