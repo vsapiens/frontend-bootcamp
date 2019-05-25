b97162f75ab5802dd50965dba287c284eec7a9a0
vsapiens <erick.frank@icloud.com>
2019-05-24 21:36:59 -0500

    Rendering and Event handling

---
+++ market-app/src/components/counter.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tags", "tag2", "tag3"]
+  };
+  /*
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
+  };
+  
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  */
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button
+          onClick={() => {
+            this.handleIncrement(product);
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/*this.state.tags.length === 0 && "Please add one tag!"*/}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p> There are no tags! </p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
